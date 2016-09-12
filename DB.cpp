a6ef196ef88ff22efd581a389c13d8a3a40c14e2
jcaracciolo <jcaracciolo@itba.edu.ar>
2016-09-12 16:53:51 -0300

    DB 2

--- DB.cpp
+++ DB.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <set>
-#include "DB.h"
+#include <stdint.h>
 
+typedef struct{
+    uint64_t high;
+    uint64_t low;
+} UUID;
+
+
+// {
+//
+//UUID newUUID(uint64_t high,uint64_t low);
+//
+//void DBadd(UUID id);
+//
+//void DBremove(UUID id);
+//
+//int DBcontains(UUID id);
+//
+//}
 
 bool operator<(const UUID &a,const UUID &b){
     return (a.high==b.high)? a.low<b.low : a.high < b.high ;
 }
 
 static std::set<UUID> database;
-
 
 extern "C" UUID newUUID(uint64_t high,uint64_t low){
     UUID ans;
@@ -27,18 +43,4 @@ extern "C" void DBremove(UUID id){
 
 extern "C" int DBcontains(UUID id){
     return (int)(database.find(id) != database.end());
-}
-
-int main()
-{
-
-    database.insert(newUUID(1,2));
-    database.insert(newUUID(1,2));
-    database.insert(newUUID(1,2));
-
-    UUID a=newUUID(1,2313213213);
-    DBadd(a);
-
-    std::cout << DBcontains(a)  <<"\n";
-    std::cout << a.low  <<"\n";
 }
--- DB.h
+++ DB.h
@@ -1,7 +1,7 @@
 //
 // Created by juanfra on 12/09/16.
 //
-#include <ctype.h>
+#include <stdint.h>
 
 #ifndef SO_TP1_DB_H
 #define SO_TP1_DB_H
@@ -11,12 +11,14 @@ typedef struct{
     uint64_t  low;
 } UUID;
 
-extern "C" UUID newUUID(uint64_t high,uint64_t low);
+UUID newUUID(uint64_t high,uint64_t low);
 
-extern "C" void DBadd(UUID id);
+void DBadd(UUID id);
 
-extern "C" void DBremove(UUID id);
+void DBremove(UUID id);
 
-extern "C" int DBcontains(UUID id);
+int DBcontains(UUID id);
+
+
 
 #endif //SO_TP1_DB_H
